4020358445d4409c49ec730f1502f9ba8496676b
SomeshAgrawal1318 <agrawalsomesh1805@gmail.com>
2026-07-23 13:44:32 +0800

    Project set up, MongoDB setup, Backend paths list, and frontend wireframes

---
+++ server/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDB from './config/dbConnection.js';
+
+const port = process.env.PORT || 5000;
+const app = express();
+connectDB();
+
+app.listen(port, ()=>{
+    console.log(`Server running on port ${port}`);
+});
