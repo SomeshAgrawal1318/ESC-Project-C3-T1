4f7d06e2c0987478726be5692cda22be023fef55
Somesh Agrawal <agrawalsomesh1805@gmail.com>
2026-07-27 03:34:23 +0800

    Wire frames set 2 and sample API

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/dbConnection.js';
@@ -10,22 +11,17 @@ import errorHandler from './middleware/errorHandler.js';
 const port = process.env.PORT || 5000;
 const app = express();
 connectDB();
-
+app.use(cors());
 app.use(express.json());
 app.use('/api/samples', samples);
 app.use('/api/students', students);
 app.use('/api/recommendation', recommendation);
 app.use(errorHandler);
-
-
-
-app.listen(port, ()=>{
-    console.log(`Server running on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
-
 
 // GET http://localhost:4000/students/
 // req.body.
 // res.status(404);
 // throw new Error("asdfasdf");
-
